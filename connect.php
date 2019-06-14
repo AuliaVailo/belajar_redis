3aefd6f5b915ba125b7c6b65a60ee88c6edb8db7
AuliaVailo <abdul.haq.aulia@gmail.com>
2019-06-14 20:27:28 +0700

    Test Koneksi Ke Server Redis di Centos 7

--- connect.php
+++ connect.php
@@ -6,6 +6,13 @@ use Predis\Client;
 
 try{
     $predis = new Client();
+
+    $predis = new Client(array(
+        "scheme" => "tcp",
+        "host"  => "192.168.22.129",
+        "port" => 6379
+    ));
+
     printf('Connection to redis has been established...\n');
 }
 catch(Exception $e){
